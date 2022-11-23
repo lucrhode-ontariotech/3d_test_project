7019fa113fda677a15dd6bc0a34a34c258737fc3
lucrhode <luc.rhode@ontariotechu.net>
2022-11-22 20:40:07 -0500

    Player/Camera Movement
    Added player movement through scripting. Added a separate player camera parented to the player object to allow for better camera positioning.

--- Assets/Scripts/PlayerBehaviour.cs
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class PlayerBehaviour : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float speedMultiplier = 7f;
+    public float rotationMultiplier = 165f;
+
+    float hAxisInput;
+    float vAxisInput;
+    Rigidbody objRB;
+
     void Start()
     {
-        
+        objRB = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        vAxisInput = -(Input.GetAxis("Vertical")) * speedMultiplier;
+        hAxisInput = Input.GetAxis("Horizontal") * rotationMultiplier;
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 rotation = Vector3.up * hAxisInput;
+        Quaternion angleRot = Quaternion.Euler(rotation * Time.fixedDeltaTime);
+        objRB.MovePosition(this.transform.position + this.transform.forward * vAxisInput * Time.fixedDeltaTime);
+        objRB.MoveRotation(objRB.rotation * angleRot);
     }
 }
